019c1ca33a5cbc97d43ffdc13c1e1650aba2f96b
Marek Solarz <solarz.mar@gmail.com>
2020-01-11 11:46:09 +0100

    providing whole world ends the game

--- src/main/java/Hangmangame.java
+++ src/main/java/Hangmangame.java
@@ -1,47 +1,52 @@
 import java.util.Scanner;
 
 public class Hangmangame {
-    private static String word = Wordbank.wordbank[(int) (Math.random()* Wordbank.wordbank.length)];
-    private static String asterisk = word.replaceAll("[a-zA-Z]", "*");
+    private static String word = Wordbank.wordbank[(int) (Math.random() * Wordbank.wordbank.length)];
+    private static String hiddenletter = word.replaceAll("[a-zA-Z]", "*");
     private static int attempt = 0;
 
 
     public static void main(String[] args) {
         Scanner letter = new Scanner(System.in);
-        while (attempt < 7 && asterisk.contains("*")) {
+        while (attempt < 7 && hiddenletter.contains("*")) {
             System.out.println("Guess any letter in the word");
-            System.out.println(asterisk);
+            System.out.println(hiddenletter);
             String guess = letter.next();
-            if(hang(guess)){
+            if (hang(guess)) {
                 System.out.println("Correct! You win! The word was " + word);
                 System.out.println("Do you want to play again ?");
+                break;
             }
         }
     }
+
     public static boolean hang(String guess) {
-        String newasterisk = "";
+        String newletter = "";
         for (int i = 0; i < word.length(); i++) {
+            if (guess.equals(word)){
+                return true;
+            }else{
+            }
             if (word.charAt(i) == guess.charAt(0)) {
-                newasterisk += guess.charAt(0);
-            } else if (asterisk.charAt(i) != '*') {
-                newasterisk += word.charAt(i);
+                newletter += guess.charAt(0);
+            } else if (hiddenletter.charAt(i) != '*') {
+                newletter += word.charAt(i);
             } else {
-                newasterisk += "*";
+                newletter += "*";
             }
         }
-        if (asterisk.equals(newasterisk)) {
+        if (hiddenletter.equals(newletter)) {
             attempt++;
             hangmanImage();
         } else {
-            asterisk = newasterisk;
+            hiddenletter = newletter;
         }
-        if (asterisk.equals(word)) {
+        if (hiddenletter.equals(word)) {
             return true;
-        }else
+        } else
             return false;
 
-        }
-
+    }
 
     public static void hangmanImage() {
         if (attempt == 1) {
@@ -125,6 +130,7 @@ public class Hangmangame {
             System.out.println("___|___      /   \\");
             System.out.println("GAME OVER! The word was " + word);
             System.out.println("Do you want to play again ?");
+
         }
     }
 }
